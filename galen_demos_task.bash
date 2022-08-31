d542d40627cf0ad1c90c35e71bf6a68cf0673bbc
JunHaoChng <jun_hao_chng@cgh.com.sg>
2022-08-31 10:49:16 +0000

    Testing Kubernetes setup at galen

--- galen_demos_task.bash
+++ galen_demos_task.bash
@@ -3,18 +3,21 @@ source ~/jh_rmf_ws/install/setup.bash && \
   export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp && \
   export CYCLONEDDS_URI=file:///home/jh/wg_cyclonedds.xml && \
   export ROS_DOMAIN_ID=15 && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_1 l2_4 -n 1 --use_sim_time && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_5 l2_7 -n 1 --use_sim_time && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_8 l2_10 -n 1 --use_sim_time && \
-  sleep 100 && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_2 l2_3 -n 1 --use_sim_time && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_6 l2_6 -n 1 --use_sim_time && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_9 l2_9 -n 1 --use_sim_time && \
-  sleep 100 && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_3 l2_1 -n 1 --use_sim_time && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_7 l2_5 -n 1 --use_sim_time && \
-  ros2 run rmf_demos_tasks dispatch_patrol -p l1_10 l2_8 -n 1 --use_sim_time && \
-  sleep 100 #&& \
+  ros2 run rmf_demos_tasks dispatch_patrol -p m_start m_end -n 1 && \
+  ros2 run rmf_demos_tasks dispatch_patrol -p n_start n_try -n 1 
+
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_1 l2_4 -n 1 --use_sim_time && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_5 l2_7 -n 1 --use_sim_time && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_8 l2_10 -n 1 --use_sim_time && \
+  # sleep 100 && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_2 l2_3 -n 1 --use_sim_time && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_6 l2_6 -n 1 --use_sim_time && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_9 l2_9 -n 1 --use_sim_time && \
+  # sleep 100 && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_3 l2_1 -n 1 --use_sim_time && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_7 l2_5 -n 1 --use_sim_time && \
+  # ros2 run rmf_demos_tasks dispatch_patrol -p l1_10 l2_8 -n 1 --use_sim_time && \
+  # sleep 100 #&& \
   # ros2 run rmf_demos_tasks dispatch_patrol -p l2_1 l1_3  -n 1 --use_sim_time && \
   # ros2 run rmf_demos_tasks dispatch_patrol -p l2_5 l1_7  -n 1 --use_sim_time && \
   # ros2 run rmf_demos_tasks dispatch_patrol -p l2_8 l1_10 -n 1 --use_sim_time && \
